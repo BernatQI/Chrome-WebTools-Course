fe8f3eeb05058b88a46d47cc43de5ec39f4b38cd
BernatQi <bernatqipro@gmail.com>
2022-11-06 11:13:06 +0100

    Creating console.group

---
+++ Workspace Filesystem/app.js
@@ -0,0 +1,15 @@
+function onClickEvent() {
+  let count = 1;
+  alert(`You completed ` + count + ` exercices`);
+  count = nestedCall(count);
+  alert(`You completed ` + count + ` exercices`);
+}
+
+function nestedCall(count) {
+  
+  //document.querySelector("body > ul > li:nth-child(2)").remove();
+  console.log(`Reached nestedCall`);
+  return count + 7;
+}
+
+console.log(`Hello`);
--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 function onClickEvent() {
+  console.group(`onClickEvent`);
   let count = 1;
-  alert(`You completed ` + count + ` exercices`);
+  //alert(`You completed ` + count + ` exercices`);
+  console.warn('warn');
   count = nestedCall(count);
-  alert(`You completed ` + count + ` exercices`);
+  //alert(`You completed ` + count + ` exercices`);
+  console.error('error');
+  console.groupEnd(`onClickEvent`);
 }
 
 function nestedCall(count) {
-  
+  console.group(`nestedCall`);
   document.querySelector("body > ul > li:nth-child(2)").remove();
   console.log(`Reached nestedCall`);
-  return count + 6;
+  console.groupEnd(`nestedCall`);
+  return count + 7;
 }
