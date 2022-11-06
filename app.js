bbeacece3da9da015d6151f5b03087fcbc669e90
BernatQi <bernatqipro@gmail.com>
2022-11-06 09:00:40 +0100

    Changing app.js to show count

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 function onClickEvent() {
-  alert(`You are awesome!`);
+  let count = 1;
+  alert(`You completed ` + count + ` exercices`);
+  count = nestedCall(count);
+  alert(`You completed ` + count + ` exercices`);
+}
+
+function nestedCall(count) {
+  console.log(`Reached nestedCall`);
+  return count + 6;
 }
